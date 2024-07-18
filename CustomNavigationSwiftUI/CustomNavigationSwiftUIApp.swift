86638991198847d9580e32fe0ca35d60a51cbd9b
Nikita12G <gulyi08@mail.ru>
2024-07-18 17:53:55 +0700

    Initial Commit

---
+++ CustomNavigationSwiftUI/CustomNavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavigationSwiftUIApp.swift
+//  CustomNavigationSwiftUI
+//
+//  Created by Никита Гуляев on 18.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
